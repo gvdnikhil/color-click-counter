9d59ee5408295c89f5dbe86d8cc8108c9267c722
Nikhilgvd <86193313+gvdnikhil@users.noreply.github.com>
2024-05-29 20:46:15 +0530

    Add files via upload
    2md commit using the object state, spread function, passing new object using spread, using callback to movestateupdate

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -1,60 +1,64 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function LudoBoard() {
+  const [moves, movestate] = useState({ blue: 0, red: 0, yellow: 0, green: 0 });
 
+  const updateblue = () => {
+   
+    movestate((prevmoves) => {
+        return ({...prevmoves ,blue: prevmoves.blue+1 });
+    });
 
-    const [redcount,redcountstate] = useState(0);
-  
-    function redclick()
-    {
-      redcountstate(redcount+1);
-      console.log("red++ count " , redcount);
-    }
-  
-    const [bluecount,bluecountstate] = useState(0);
-  
-    function blueclick()
-    {
-      bluecountstate(bluecount+1);
-      console.log("blue++ count " , bluecount);
-    }
-  
-    // function redclick()
-    // {
-    //   redcountstate(redcount+1);
-    //   console.log("red++ count " , redcount);
-    // }
-  
-    // function redclick()
-    // {
-    //   redcountstate(redcount+1);
-    //   console.log("red++ count " , redcount);
-    // }
-  
-  
-  
-  
-  
-    return (
-  
-      <>
+    console.log(moves.blue);
+  };
+
+  const updateyellow = () => {
+   
+    movestate((prevmoves) => {
+        return ({...prevmoves ,yellow: prevmoves.yellow+1 });
+    });
+
+    console.log(moves.yellow);
+  };
+
+  const updategreen = () => {
+   
+    movestate((prevmoves) => {
+        return ({...prevmoves ,green: prevmoves.green+1 });
+    });
+
+    console.log(moves.green);
+  };
+
+  const updatered = () => {
+   
+    movestate((prevmoves) => {
+        return ({...prevmoves ,red: prevmoves.red+1 });
+    });
+
+    console.log(moves.red);
+  };
+
+  return (
+    <>
       <h1>Ludo Game</h1>
-        <p>Blue Moves: {bluecount} </p>
-        <button onClick={blueclick} style={{backgroundColor :"blue"}}>+1</button>
-        <p>Yellow Moves:  </p>
-        <button  style={{backgroundColor :"yellow"}}>+1</button>
-        <p>Red Moves: {redcount}  </p>
-        <button onClick={redclick} style={{backgroundColor :"red"}}>+1</button>
-        <p>Green Moves:  </p>
-        <button style={{backgroundColor :"green"}}>+1</button>
-  
-  
-  
-      </>
-      
-     
-          
-    )
-  }
-  
-  export default LudoBoard
+      <p>Blue Moves: {moves.blue} </p>
+      <button onClick={updateblue} style={{ backgroundColor: "blue" }}>
+        +1
+      </button>
+
+      <p>Yellow Moves: {moves.yellow} </p>
+      <button  onClick={updateyellow} style={{ backgroundColor: "yellow" }}>+1</button>
+
+      <p>Red Moves: {moves.red} </p>
+      <button onClick={updatered} style={{ backgroundColor: "red" }}>
+        +1
+      </button>
+
+      <p>Green Moves: {moves.green} </p>
+      <button onClick={updategreen} style={{ backgroundColor: "green" }}>+1</button>
+    </>
+  );
+}
+
+export default LudoBoard;
